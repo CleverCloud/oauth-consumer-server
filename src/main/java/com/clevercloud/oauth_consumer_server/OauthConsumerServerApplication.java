660d34e2f6cc37f60ef94c714c29940a1c802d13
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2025-01-19 18:45:30 +0100

    Let's try again, all files indented with spaces and no untrimmed lines

--- src/main/java/com/clevercloud/oauth_consumer_server/OauthConsumerServerApplication.java
+++ src/main/java/com/clevercloud/oauth_consumer_server/OauthConsumerServerApplication.java
@@ -25,71 +25,71 @@ import org.slf4j.LoggerFactory;
 @SpringBootApplication
 public class OauthConsumerServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OauthConsumerServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(OauthConsumerServerApplication.class, args);
+  }
 
 }
 
 @Controller
 class OAuthController {
-	@Value("${app.url:}")
-	String appURL;
+  @Value("${app.url:}")
+  String appURL;
 
 
   private static final Logger logger = LoggerFactory.getLogger(OAuthController.class);
-  
-	private ConcurrentHashMap<String, OAuth10aService> services = new ConcurrentHashMap<>();
-	private ConcurrentHashMap<String, OAuth1RequestToken> requestTokens = new ConcurrentHashMap<>();
+ 
+  private ConcurrentHashMap<String, OAuth10aService> services = new ConcurrentHashMap<>();
+  private ConcurrentHashMap<String, OAuth1RequestToken> requestTokens = new ConcurrentHashMap<>();
 
-	@RequestMapping(method = RequestMethod.GET, path = "/")
-	public String startDance(@RequestParam("consumerKey") String consumerKey,
-			@RequestParam("consumerSecret") String consumerSecret, HttpServletResponse httpServletResponse) {
+  @RequestMapping(method = RequestMethod.GET, path = "/")
+  public String startDance(@RequestParam("consumerKey") String consumerKey,
+      @RequestParam("consumerSecret") String consumerSecret, HttpServletResponse httpServletResponse) {
 
-		// Create an Oauth service provider using consumerKey and consumerSecret, and 
-		// the Clever Cloud API definition for endpoints
-		OAuth10aService service = new ServiceBuilder(consumerKey)
-				.apiSecret(consumerSecret)
-				.callback(appURL + "callback")
-				.build(OauthApi.instance());
+    // Create an Oauth service provider using consumerKey and consumerSecret, and
+    // the Clever Cloud API definition for endpoints
+    OAuth10aService service = new ServiceBuilder(consumerKey)
+        .apiSecret(consumerSecret)
+        .callback(appURL + "callback")
+        .build(OauthApi.instance());
 
-		// fetches a request token from the service provider and builds
-		// a url based on AUTHORIZE_WEBSITE_URL and CALLBACK_URL to
-		// which your app must now send the user
-		try {
-			logger.debug("Getting request token ...");
-			OAuth1RequestToken requestToken = service.getRequestToken();
+    // fetches a request token from the service provider and builds
+    // a url based on AUTHORIZE_WEBSITE_URL and CALLBACK_URL to
+    // which your app must now send the user
+    try {
+      logger.debug("Getting request token ...");
+      OAuth1RequestToken requestToken = service.getRequestToken();
 
-			services.put(requestToken.getToken(),service);
-			requestTokens.put(requestToken.getToken(),requestToken);
+      services.put(requestToken.getToken(),service);
+      requestTokens.put(requestToken.getToken(),requestToken);
 
-			String url = service.getAuthorizationUrl(requestToken);
-			logger.debug(String.format("URL : %s", url));
-			return String.format("redirect:%s", url);
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-			return e.getMessage();
-		}
-	}
+      String url = service.getAuthorizationUrl(requestToken);
+      logger.debug(String.format("URL : %s", url));
+      return String.format("redirect:%s", url);
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return e.getMessage();
+    }
+  }
 
 
-	@RequestMapping(method = RequestMethod.GET,  path = "/callback")
-	public @ResponseBody
-	String danceCallback(@RequestParam("oauth_token") String oauth_token, @RequestParam("oauth_verifier") String oauth_verifier, @RequestParam("user") String user) {
+  @RequestMapping(method = RequestMethod.GET,  path = "/callback")
+  public @ResponseBody
+  String danceCallback(@RequestParam("oauth_token") String oauth_token, @RequestParam("oauth_verifier") String oauth_verifier, @RequestParam("user") String user) {
 
-		try {
-			logger.debug(String.format("OAuth token : [%s]", oauth_token));
-			logger.debug(String.format("OAuth verifier : [%s]", oauth_verifier));
-			logger.debug(String.format("User : [%s]", user));
-			
-			OAuth10aService service = services.get(oauth_token);
-			OAuth1RequestToken requestToken = requestTokens.get(oauth_token);
-			OAuth1AccessToken accessToken = service.getAccessToken(requestToken, oauth_verifier);
-			logger.debug("Access Token: " + accessToken);
-			return "Your token : " + accessToken.getToken() + "\nYour token secret : " + accessToken.getTokenSecret();
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-			return e.getMessage();
-		}
-	} 
+    try {
+      logger.debug(String.format("OAuth token : [%s]", oauth_token));
+      logger.debug(String.format("OAuth verifier : [%s]", oauth_verifier));
+      logger.debug(String.format("User : [%s]", user));
+      
+      OAuth10aService service = services.get(oauth_token);
+      OAuth1RequestToken requestToken = requestTokens.get(oauth_token);
+      OAuth1AccessToken accessToken = service.getAccessToken(requestToken, oauth_verifier);
+      logger.debug("Access Token: " + accessToken);
+      return "Your token : " + accessToken.getToken() + "\nYour token secret : " + accessToken.getTokenSecret();
+    } catch (Exception e) {
+      logger.error(e.getMessage());
+      return e.getMessage();
+    }
+  }
 }
